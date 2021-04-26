c4c59e6cf7664729dd06c9fa13531260f668b8b6
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2021-04-26 23:11:24 +0200

    decor 1

--- src/game.ts
+++ src/game.ts
@@ -2,13 +2,22 @@ import resources from "./resources"
 import { loadPictures } from "./modules/nftBuilder"
 
 const building = new Entity()
-building.addComponent(
-  new Transform({
-    position: new Vector3(0, 0, 0),
-  })
-)
+const transform =   new Transform({
+  position: new Vector3(0, 0, 0),
+})
+building.addComponent(transform)
 building.addComponent(resources.models.building)
 engine.addEntity(building)
+
+const liquidFloor = new Entity()
+liquidFloor.addComponent(new Transform({position: new Vector3(0,0.02,0)}))
+liquidFloor.addComponent(new GLTFShape('models/LiquidFloor.glb'))
+engine.addEntity(liquidFloor)
+
+const liquidFloorInverse = new Entity()
+liquidFloorInverse.addComponent(new Transform({position: new Vector3(130,0,61), rotation: Quaternion.Euler(0,180,0)}))
+liquidFloorInverse.addComponent(new GLTFShape('models/LiquidFloor.glb'))
+//engine.addEntity(liquidFloorInverse)
 
 // Music
 const streamSource = new Entity()
