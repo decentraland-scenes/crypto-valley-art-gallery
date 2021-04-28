09b4a7fe07238e980d3a44d76d306d73215db65f
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2021-04-28 03:38:33 +0200

    slime tree test

--- src/game.ts
+++ src/game.ts
@@ -9,6 +9,7 @@ building.addComponent(transform)
 building.addComponent(resources.models.building)
 engine.addEntity(building)
 
+//Floor
 const liquidFloor = new Entity()
 liquidFloor.addComponent(new Transform({position: new Vector3(0,0.02,0)}))
 liquidFloor.addComponent(new GLTFShape('models/LiquidFloor.glb'))
@@ -18,6 +19,68 @@ const liquidFloorInverse = new Entity()
 liquidFloorInverse.addComponent(new Transform({position: new Vector3(130,0,61), rotation: Quaternion.Euler(0,180,0)}))
 liquidFloorInverse.addComponent(new GLTFShape('models/LiquidFloor.glb'))
 //engine.addEntity(liquidFloorInverse)
+
+//Slime test 
+const pinkSlime = new GLTFShape('models/Slime_01.glb')
+const Slime1 = new Entity()
+Slime1.setParent(building)
+Slime1.addComponent(new Transform({position: new Vector3(4,0.2,12), rotation: Quaternion.Euler(0,90,0)}))
+Slime1.addComponent(pinkSlime)
+engine.addEntity(Slime1)
+
+const Slime2 = new Entity()
+Slime2.setParent(building)
+Slime2.addComponent(new Transform({position: new Vector3(4,0.2,31), rotation: Quaternion.Euler(0,90,0)}))
+Slime2.addComponent(pinkSlime)
+engine.addEntity(Slime2) 
+
+const Slime3 = new Entity()
+Slime3.setParent(building)
+Slime3.addComponent(new Transform({position: new Vector3(4,0.2,50), rotation: Quaternion.Euler(0,90,0)}))
+Slime3.addComponent(pinkSlime)
+engine.addEntity(Slime3)
+
+const Slime4 = new Entity()
+Slime4.setParent(building)
+Slime4.addComponent(new Transform({position: new Vector3(14,0.2,60), rotation: Quaternion.Euler(0,0,0)}))
+Slime4.addComponent(pinkSlime)
+engine.addEntity(Slime4)
+
+const Slime5 = new Entity()
+Slime5.setParent(building)
+Slime5.addComponent(new Transform({position: new Vector3(39,0.2,60), rotation: Quaternion.Euler(0,0,0)}))
+Slime5.addComponent(pinkSlime)
+engine.addEntity(Slime5)
+
+const Slime6 = new Entity()
+Slime6.setParent(building)
+Slime6.addComponent(new Transform({position: new Vector3(64,0.2,60), rotation: Quaternion.Euler(0,0,0)}))
+Slime6.addComponent(pinkSlime)
+engine.addEntity(Slime6)
+
+const Slime7 = new Entity()
+Slime7.setParent(building)
+Slime7.addComponent(new Transform({position: new Vector3(88.5,0.2,60), rotation: Quaternion.Euler(0,0,0)}))
+Slime7.addComponent(pinkSlime)
+engine.addEntity(Slime7)
+
+const Slime8 = new Entity()
+Slime8.setParent(building)
+Slime8.addComponent(new Transform({position: new Vector3(113,0.2,60), rotation: Quaternion.Euler(0,0,0)}))
+Slime8.addComponent(pinkSlime)
+engine.addEntity(Slime8)
+
+const Slime9 = new Entity()
+Slime9.setParent(building)
+Slime9.addComponent(new Transform({position: new Vector3(124,0.2,49), rotation: Quaternion.Euler(0,90,0)}))
+Slime9.addComponent(pinkSlime)
+engine.addEntity(Slime9)
+
+const Slime10 = new Entity()
+Slime10.setParent(building)
+Slime10.addComponent(new Transform({position: new Vector3(121,0.2,3.5), rotation: Quaternion.Euler(0,0,0)}))
+Slime10.addComponent(pinkSlime)
+engine.addEntity(Slime10)
 
 // Music
 const streamSource = new Entity()
