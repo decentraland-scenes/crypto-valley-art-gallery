3a379b4250a6baee49f9aa7674c49cb8381e8cba
KatherineJoelle <53322457+KatherineJoelle@users.noreply.github.com>
2021-07-02 21:44:31 +0200

    wave test

--- src/game.ts
+++ src/game.ts
@@ -20,3 +20,17 @@ engine.addEntity(streamSource)
 
 // Pictures
 loadPictures(building)
+
+const waveVideoClip = new VideoClip("https://player.vimeo.com/external/570332974.hd.mp4?s=a99ad0af8bcc458c91a7988b53da159615af8ded&profile_id=175")
+const waveVideoTexture = new VideoTexture(waveVideoClip)
+const waveMaterial = new BasicMaterial()
+waveMaterial.texture = waveVideoTexture
+
+let waveScreen = new Entity()
+waveScreen.addComponent(new PlaneShape())
+waveScreen.addComponent(new Transform({position: new Vector3(8,0,8), rotation: Quaternion.Euler(0,0,0), scale: new Vector3(10,10,10)}))
+waveScreen.addComponent(waveMaterial)
+waveVideoTexture.play()
+waveVideoTexture.loop = true
+
+engine.addEntity(waveScreen)
